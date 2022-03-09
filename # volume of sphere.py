736dab2186799c14240ab1eb395e74d8be63e198
Hunu Emmanuella <100598015+Ellababe@users.noreply.github.com>
2022-03-09 17:12:15 +0000

    Add files via upload

---
+++ # volume of sphere.py
@@ -0,0 +1,51 @@
+# volume of a sphere
+# question 1
+
+r = 5
+g= 3.14
+
+volume = 4/3*g*r**3
+print(volume)
+
+
+
+# question 2
+# cost
+
+c  = 24.95
+# c = coverprice for one book
+
+d = 0.4 
+# d = discount
+
+f = 3.00
+# f = first shipping
+
+a = 0.75
+# a = additional shipping
+
+u = 60
+# u = units
+
+
+# total cost for shipping
+bat = (f + (u - 1) * a)
+print(bat)
+
+# total cost for all books
+x = c * u
+print(x)
+
+y = x * d
+print(y)
+
+z = x - y 
+print(z)
+
+# wholesale cost
+# summation of the shipping fee and Total cost of books will give the wholesale cost.
+
+p
+
+
+
